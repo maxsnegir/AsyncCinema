64ceac2e58e3f48c57b705fdf8865037f629d9ba
Max Snegirev <goshasnegir@icloud.com>
2022-01-16 22:11:35 +0500

    Добавлены тесты для тестирования эндпоинта /film

--- tests/functional/settings.py
+++ tests/functional/settings.py
@@ -1,9 +1,12 @@
-from pydantic import BaseSettings
+from pathlib import Path
+
+from pydantic import BaseSettings, Field
 
 
 class Settings(BaseSettings):
-    SERVICE_URL: str = 'http://localhost:8001'  # ToDO поправить перед деплоем
+    SERVICE_URL: str = Field('http://localhost:8001')  # ToDO поправить перед деплоем
     API_URL = "/api/v1"
-    INDEX_MAP_PATH = "../testdata/es_schemas/"
-    TEST_DATA_PATH = "../testdata/"
+    INDEX_MAP_PATH: Path = Field("tests/functional/testdata/es_schemas/")
+    TEST_DATA_PATH: Path = Field("tests/functional/testdata/")
     INDEXES = ("movies", "genres", "persons")
+    es_host: str = Field('http://127.0.0.1:9200', env='ELASTIC_HOST')
