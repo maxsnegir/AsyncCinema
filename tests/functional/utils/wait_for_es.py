cc13d4c07f078c79f0dfdb3b69a1bf156fffdb43
yar <kotsar.yaroslav@gmail.com>
2022-01-24 16:52:00 +0000

    #44 p.5 add timeout to wait_connection utils

--- tests/functional/utils/wait_for_es.py
+++ tests/functional/utils/wait_for_es.py
@@ -1,9 +1,12 @@
 import logging
 import os
-from time import sleep
+from time import sleep, time
 
 from dotenv import load_dotenv
 from elasticsearch import Elasticsearch
+
+
+WAIT_TIME=120
 
 load_dotenv()
 logger = logging.getLogger()
@@ -12,9 +15,13 @@ logger = logging.getLogger()
 def wait_for_es(es_client: Elasticsearch) -> None:
     """ Ждем пока es станет доступным """
 
+    waiting_start = time()
     while not es_client.ping():
         logger.info("Waiting for Elasticsearch")
         sleep(2)
+        if (time() - waiting_start) > WAIT_TIME:
+            logger.info("Elasticsearch connection timeout")
+            raise ConnectionError
     logger.info("Elasticsearch connected")
 
 
