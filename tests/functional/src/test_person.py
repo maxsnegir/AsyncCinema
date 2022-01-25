7cd1d765f8b4b537c4984e4e16571a09254ffe4d
yar <kotsar.yaroslav@gmail.com>
2022-01-25 07:54:06 +0000

    #44 p.9 add pytestmark

--- tests/functional/src/test_person.py
+++ tests/functional/src/test_person.py
@@ -5,6 +5,9 @@ import pytest
 from ..utils.constants import TestErrors as err
 from ..utils.helper import get_redis_key_by_params
 
+# All test coroutines will be treated as marked
+pytestmark = pytest.mark.asyncio
+
 
 @pytest.fixture
 def query_person_params() -> dict:
@@ -18,7 +21,6 @@ def query_person_params() -> dict:
 
 
 class TestPerson:
-    @pytest.mark.asyncio
     async def test_person_list_without_params(self, create_test_data, make_get_request, redis_client, settings,
                                               query_person_params):
         """Тест эндпоинта /person/search"""
@@ -31,7 +33,6 @@ class TestPerson:
         assert await redis_client.get(
             get_redis_key_by_params(settings.PERSON_INDEX, query_person_params)), err.REDIS_404
 
-    @pytest.mark.asyncio
     async def test_person_page_number_param(self, create_test_data, make_get_request, query_person_params, settings,
                                             redis_client):
         """Тест параметра ?page_number"""
@@ -51,19 +52,16 @@ class TestPerson:
         assert isinstance(response_with_first_number.body, list), err.WRONG_RESPONSE_BODY
         assert response.body != response_with_first_number.body, "Значения 1-ой и 2-ой страницы не должны совпадать"
 
-    @pytest.mark.asyncio
     async def test_person_page_number_param_page1M(self, create_test_data, make_get_request, query_person_params):
         query_person_params["page_number"] = 1_000_000
         response_with_big_page_number = await make_get_request("/person/search", params=query_person_params)
         assert response_with_big_page_number.status == HTTPStatus.UNPROCESSABLE_ENTITY, err.WRONG_GTE_PAGE_SIZE
 
-    @pytest.mark.asyncio
     async def test_person_page_number_param_page0(self, create_test_data, make_get_request, query_person_params):
         query_person_params["page_number"] = 0
         response_with_zero_page_number = await make_get_request("/person/search", params=query_person_params)
         assert response_with_zero_page_number.status == HTTPStatus.UNPROCESSABLE_ENTITY, err.WRONG_LTE_PAGE_SIZE
 
-    @pytest.mark.asyncio
     async def test_person_page_size_param(self, create_test_data, make_get_request, query_person_params, redis_client,
                                           settings):
         """Тест параметра ?page_size"""
@@ -75,19 +73,16 @@ class TestPerson:
         assert await redis_client.get(
             get_redis_key_by_params(settings.PERSON_INDEX, query_person_params)), err.REDIS_404
 
-    @pytest.mark.asyncio
     async def test_person_page_size_param_size1M(self, create_test_data, make_get_request, query_person_params):
         query_person_params["page_size"] = 1_000_000
         response_with_big_page_size = await make_get_request("/person/search", params=query_person_params)
         assert response_with_big_page_size.status == HTTPStatus.UNPROCESSABLE_ENTITY, err.WRONG_GTE_PAGE_SIZE
 
-    @pytest.mark.asyncio
     async def test_person_page_size_param_size0(self, create_test_data, make_get_request, query_person_params):
         query_person_params["page_size"] = 0
         response_with_zero_page_size = await make_get_request("/person/search", params=query_person_params)
         assert response_with_zero_page_size.status == HTTPStatus.UNPROCESSABLE_ENTITY, err.WRONG_LTE_PAGE_SIZE
 
-    @pytest.mark.asyncio
     async def test_person_by_id(self, create_test_data, make_get_request, settings, redis_client):
         """Тест эндпоинта /person/{person_id}"""
 
@@ -99,13 +94,11 @@ class TestPerson:
         assert response.body.get("uuid") is not None, "У персонажа отсутствует uuid"
         assert await redis_client.get(settings.PERSON_INDEX + ":" + existing_person), err.REDIS_404
 
-    @pytest.mark.asyncio
     async def test_person_by_id_nonexistent(self, create_test_data, make_get_request):
         nonexistent_person = "40415390-07d6-46a5-942e-24819250f2cb"
         response_for_nonexistent_person = await make_get_request(f"/film/{nonexistent_person}")
         assert response_for_nonexistent_person.status == HTTPStatus.NOT_FOUND, err.WRONG_STATUS
 
-    @pytest.mark.asyncio
     async def test_person_search(self, create_test_data, make_get_request, query_person_params):
         """Тест энпоинта /person/search"""
 
@@ -115,7 +108,6 @@ class TestPerson:
         assert len(response.body) == 1, err.WRONG_LEN
         assert response.body[0]["uuid"] == "bddfebe2-abfb-4e2f-a49e-22b3d9818a2c", 'У персоны нет UUID'
 
-    @pytest.mark.asyncio
     async def test_person_films(self, create_test_data, make_get_request, redis_client, settings, query_person_params):
         """ Тест person/<person_uuid>film/ """
 
