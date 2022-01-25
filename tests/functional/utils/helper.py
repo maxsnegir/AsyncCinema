491dab7c49f00f273bac416aa92100831faa8448
yar <kotsar.yaroslav@gmail.com>
2022-01-25 07:47:29 +0000

    #44 p.8 add asyncio.sleep

--- tests/functional/utils/helper.py
+++ tests/functional/utils/helper.py
@@ -1,5 +1,6 @@
 import json
 from typing import List, Generator
+from asyncio import sleep as asyncio_sleep
 
 from elasticsearch._async.helpers import async_bulk  # Noqa
 
@@ -47,6 +48,7 @@ async def wait_for_load(es_client, index):
     while not items_count:
         index_data = await es_client.count(index=index)
         items_count = index_data.get("count")
+        await asyncio_sleep(0.1)
 
 
 def get_redis_key_by_params(index_name: str, params: dict) -> str:
