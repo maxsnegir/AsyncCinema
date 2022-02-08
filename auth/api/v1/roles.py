25244749ddf067241ed5c3bf50b164567bea6f37
yar <kotsar.yaroslav@gmail.com>
2022-02-08 08:44:47 +0000

    add api to asign and remove roles, add decorator to check role

--- auth/api/v1/roles.py
+++ auth/api/v1/roles.py
@@ -1,22 +1,37 @@
+from functools import wraps
+
 from http import HTTPStatus
-from flask_jwt_extended import current_user, jwt_required
+from flask_jwt_extended import current_user, jwt_required, verify_jwt_in_request
 from flask_restful import Resource, abort
-from flask_security import roles_required
 from sqlalchemy.exc import IntegrityError
 from flask import jsonify
 from db.datastore import user_datastore
-from .parsers import role_parser
+from .parsers import role_parser, assign_role_parser
 from db import db
 from db.db_models import User
 
+def role_required(required_role:str):
+    def wrapper(fn):
+        @wraps(fn)
+        def decorator(*args, **kwargs):
+            verify_jwt_in_request()
+            role = required_role
+            if role in current_user.roles:
+                return fn(*args, **kwargs)
+            else:
+                return jsonify(message="Admins only!"), 403
+        return decorator
+    return wrapper
+
 class TestOnlyAdmin(Resource):
 
-    @roles_required('admin')
+    @role_required('admin')
     def get(self):
         return jsonify(secret='super_secret')
 
 
 class UserRole(Resource):
+    @role_required('admin')
     def post(self):
         args = role_parser.parse_args()
         try:
@@ -29,11 +44,28 @@ class UserRole(Resource):
 
     @jwt_required()
     def get(self):
-        return current_user.roles
+        return jsonify(roles=[str(role.name) for role in current_user.roles])
+
 
 class AssignRole(Resource):
+    @role_required('admin')
     def post(self):
-        args = role_parser.parse_args()
+        args = assign_role_parser.parse_args()
+        login = args.get('login')
+        user = User.get_user_by_login(login)
+        if not user:
+            return abort(HTTPStatus.NOT_FOUND, message="User not found")
+        #user = user_datastore.get_user(identifier) # Returns a user matching the specified ID or email addres
+        role = user_datastore.find_or_create_role(args.get('name'))
+        if role.name in user.roles:
+            return abort(HTTPStatus.BAD_REQUEST, message="Role already assigned")
+        user_datastore.add_role_to_user(user, role)
+        db.session.commit()
+        return jsonify(message=f'role {role.name} assigned to user {login}')
+
+    @role_required('admin')
+    def delete(self):
+        args = assign_role_parser.parse_args()
         login = args.get('login')
         user = User.get_user_by_login(login)
         if not user:
@@ -41,7 +73,7 @@ class AssignRole(Resource):
         #user = user_datastore.get_user(identifier) # Returns a user matching the specified ID or email addres
         role = user_datastore.find_or_create_role(args.get('name'))
         try:
-            user_datastore.add_role_to_user(user, role)
+            user_datastore.remove_role_from_user(user, role)
             db.session.commit()
-        except IntegrityError:
+        except IntegrityError: #TODO 404
             abort(HTTPStatus.BAD_REQUEST, message="Role already assigned")
--- auth/tests/test_auth.py
+++ auth/tests/test_auth.py
@@ -1,18 +1,19 @@
 import requests
 import pytest
-#api_v1 = Api(prefix="/api/v1")
-#api_v1.add_resource(UserRegister, '/register')
-#api_v1.add_resource(UserLogin, '/login')
-#api_v1.add_resource(UserLogout, '/logout')
-#api_v1.add_resource(RefreshToken, '/refresh')
-#api_v1.add_resource(UserInfo, '/me')
 
 url = 'http://81.163.25.103:5000' # The root of the flask app
 prefix = '/api/v1'
+
 credentials = {
     "login": "test_user",
     "email": "test_email@mail.com",
     "password": "pass123"
+}
+
+credentials_user_for_tests = {
+    "login": "test_user_for_tests",
+    "email": "test_email@yaboo.com",
+    "password": "pass12345"
 }
 
 
@@ -22,35 +23,88 @@ def token():
     data = r.json()
     return data
 
+@pytest.fixture(scope='session')
+def admin_auth_header():
+    r = requests.post(url+'/api/v1/login', params=credentials)
+    data = r.json()
+    admin_auth_header = {"Authorization": f"Bearer {data['access_token']}"}
+    return admin_auth_header
+
+
+def test_register():
+    endpoint = '/register'
+    r = requests.post(url+prefix+endpoint, params=credentials_user_for_tests)
+    assert r.status_code in (201, 400)
+
+
 def test_me(token):
+    endpoint = '/me'
     headers = {"Authorization": f"Bearer {token['access_token']}"}
-    r = requests.get(url+'/api/v1/me', params=credentials, headers=headers)
+    r = requests.get(url+prefix+endpoint, params=credentials, headers=headers)
     assert r.status_code == 200
     assert r.json()['login'] == 'test_user'
 
+
 def test_admin(token):
-    r = requests.get(url+'/api/v1/admin')
+    endpoint = '/admin'
+    headers = {"Authorization": f"Bearer {token['access_token']}"}
+    r = requests.get(url+prefix+endpoint, params=credentials, headers=headers)
     assert r.status_code == 200
 
-def test_create_admin_role():
+
+def test_create_admin_role(admin_auth_header):
     endpoint = '/role'
     params = {'name':'admin', 'description':'administrator'}
-    r = requests.post(url+prefix+endpoint, params=params)
-    assert r.status_code == 200
+    r = requests.post(url+prefix+endpoint, params=params, headers=admin_auth_header)
+    #assert r.status_code == 200
+    assert r.status_code == 400 #already exists
 
-def test_asign_admin_role():
+
+def test_asign_role(admin_auth_header):
     params = {
-        "login": "test_user",
-        "name": "admin",
+        "login": "test_user_for_tests",
+        "name": "foo",
     }
     endpoint = '/assign_role'
-    r = requests.post(url+prefix+endpoint, params=params)
-    print(r.json())
-    assert False
 
-def test_get_role(token):
-    headers = {"Authorization": f"Bearer {token['access_token']}"}
-    endpoint = '/role'
-    r = requests.get(url+prefix+endpoint, headers=headers)
+    # Remove previouly assigned role
+    requests.delete(url+prefix+endpoint, params=params, headers=admin_auth_header)
+
+    r = requests.post(url+prefix+endpoint, params=params, headers=admin_auth_header)
     print(r.json())
-    assert False
+    assert r.status_code==200
+
+
+def test_remove_admin_role(admin_auth_header):
+    params = {
+        "login": "test_user_for_tests",
+        "name": "foo",
+    }
+    endpoint = '/assign_role'
+    r = requests.delete(url+prefix+endpoint, params=params, headers=admin_auth_header)
+    assert r.status_code==200
+
+
+def test_asign_same_role_twice(admin_auth_header):
+    params = {
+        "login": "test_user_for_tests",
+        "name": "foo",
+    }
+    endpoint = '/assign_role'
+
+    # Remove previouly assigned role
+    requests.delete(url+prefix+endpoint, params=params, headers=admin_auth_header)
+
+    requests.post(url+prefix+endpoint, params=params, headers=admin_auth_header)
+    r2 = requests.post(url+prefix+endpoint, params=params, headers=admin_auth_header)
+    print(r2.json())
+    assert r2.status_code==400
+
+
+def test_get_role(admin_auth_header):
+    endpoint = '/role'
+    r = requests.get(url+prefix+endpoint, headers=admin_auth_header)
+    assert ('admin' in r.json().get('roles'))
+
+
+
