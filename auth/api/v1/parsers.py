ce73e6b7e8e358d70d42557c24a5ad6e2d482100
Max Snegirev <goshasnegir@icloud.com>
2022-02-15 17:24:13 +0300

    рефакторинг

--- auth/api/v1/parsers.py
+++ auth/api/v1/parsers.py
@@ -5,5 +5,5 @@ change_user_data_parser.add_argument('login', type=str, required=False, location
 change_user_data_parser.add_argument('email', type=str, required=False, location="form")
 
 auth_history_parser = user_namespace.parser()
-auth_history_parser.add_argument('page_number', type=int, default=0, required=False, location="form")
-auth_history_parser.add_argument('page_size', type=int, default=10, required=False, location="form")
+auth_history_parser.add_argument('page_number', type=int, default=0, required=False)
+auth_history_parser.add_argument('page_size', type=int, default=10, required=False,)
